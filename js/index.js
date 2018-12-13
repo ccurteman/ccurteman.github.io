ee9505fa22fe5153f972c66f1ac882e84951530e
Chris <c.curteman@gmail.com>
2018-12-12 21:21:24 -0800

    Updating Style + Adding Typewriter

---
+++ js/index.js
@@ -0,0 +1,64 @@
+const TypeWriter = function (txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+}
+
+// Type Method
+TypeWriter.prototype.type = function () {
+    // Current Index of wordArray
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+    //console.log(fullTxt);
+
+    // Check if deleting
+    if (this.isDeleting) {
+        // if deleting, remove a character off txt
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+    let typeSpeed = 150;
+
+    if (this.isDeleting) {
+        typeSpeed /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        typeSpeed = this.wait;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.wordIndex++;
+        typeSpeed = 500;
+    }
+
+    // If fullTxt is equal to the last word in our array, and the typewriter is on the last letter
+    if (fullTxt === ' Full Stack Web Developer' && this.txt.slice(-1) === 'r') {
+        // Finished Looping Through Array
+        // ...trailing code can go here...
+    } else {
+        setTimeout(() => this.type(), typeSpeed);
+    }
+}
+
+// Init on DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init Application
+function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words')); // data-words needs to be parsed
+    const wait = txtElement.getAttribute('data-wait');
+    let counter = this.counter;
+    // Init typewriter
+    new TypeWriter(txtElement, words, wait);
+
+}
